9f65a12ea5d9d5f4720598322a9b7a76a507a2be
MLH3 <110726668+MLH3@users.noreply.github.com>
2023-01-06 19:56:32 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const accordionEl = document.querySelectorAll(".accordion");
+const labelEl = document.querySelectorAll(".label");
+const contentEl = document.querySelectorAll(".content");
+const arrowEl = document.querySelectorAll("#arrow");
+
+for (let i = 0; i < accordionEl.length; i++) {
+  accordionEl[i].addEventListener("click", () => {
+    contentEl[i].classList.toggle("hidden");
+    labelEl[i].classList.toggle("active");
+    arrowEl[i].classList.toggle("rotate");
+  });
+}
